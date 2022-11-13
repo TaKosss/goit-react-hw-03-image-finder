5940a62989c11574f8ef9ec92467608004457d82
TaKosss <100071325+TaKosss@users.noreply.github.com>
2022-11-13 21:23:01 +0200

    Update fetchImage.js

--- src/services/fetchImage.js
+++ src/services/fetchImage.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const KEY = '29971148-c945d30420d20d2baba09e6a1'
-const url = new window.URL('https://pixabay.com/api');
+const url = new window.URL('https://pixabay.com/api/');
 url.searchParams.append('key', KEY);
 url.searchParams.append('image_type', 'photo');
 url.searchParams.append('orientation', 'horizontal');
